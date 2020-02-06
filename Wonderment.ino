8f3ccd26a0f8633b1eba58a852b82814da37a5ea
Niels <Nielsr2@gmail.com>
2020-02-06 16:04:00 +0100

    push
    push

--- Wonderment.ino
+++ Wonderment.ino
@@ -3,6 +3,7 @@
 #include <SD.h>
 #include <SPI.h>
 #include <SerialFlash.h>
+  #include <CapacitiveSensor.h>
 
 #include "chords.h"
 // **********************************************************************
@@ -72,14 +73,29 @@ AudioControlSGTL5000 sgtl5000_1;
 // AudioConnection          patchCord10(bitcrusher1, 0, mixer2, 3);
 // AudioConnection          patchCord3(string6, bitcrusher1);
 const int finger_delay = 5;
-const int hand_delay = 100;
+const int hand_delay = 200;
 
 int chordnum=0;
+int sampleSizeForCapacitiveSensor = 1;
+
+CapacitiveSensor button1 = CapacitiveSensor(0, 1);
+CapacitiveSensor button2 = CapacitiveSensor(2, 3);
+CapacitiveSensor button3 = CapacitiveSensor(4, 5);
+CapacitiveSensor button4 = CapacitiveSensor(22,17);
+// CapacitiveSensor button5 = CapacitiveSensor(3, 2);
 
 
 int x,y,z;
-int coolNumber;
-int yDown, yUp, yMid;
+int combi, coolNumber2;
+
+int down,up, mid;
+
+// **********************************************************************
+// pins
+int accelorometerPinX = 14;
+int accelorometerPinZ = 15;
+int accelorometerPinY = 16;
+
 void setup() {
         AudioMemory(15);
         sgtl5000_1.enable();
@@ -91,68 +107,130 @@ void setup() {
         mixer2.gain(1, 0.15);
         mixer2.gain(2, 0.15);
         delay(700);
-        x = analogRead(14);
-        z = analogRead(15);
-        y = analogRead(16);
-        coolNumber = sqrt(pow(x,2) + pow(y,2) + pow(z,2));
-        yDown = coolNumber * 1.1;
-        yUp =   coolNumber * 0.9;
-        yMid =  coolNumber;
+        // x =
+        // z = ;
+        // y = analogRead(accelorometerPinY);
+        coolNumber2 = sqrt(pow(analogRead(accelorometerPinX),2) + pow(analogRead(accelorometerPinY),2) + pow(analogRead(accelorometerPinZ),2));
+        down = coolNumber2 * 1.2;
+        up = coolNumber2 * 0.8;
+        mid = coolNumber2;
 }
 
 void strum_up(const float *chord, float velocity);
 void strum_dn(const float *chord, float velocity);
 
 
-int yList[10];
-int counter;
 
 boolean crossedMid = true;
 boolean direction = true; // direction up = true, down = false
 boolean played = false;
-void loop() {
-        const float *chord;
-        x = analogRead(14);
-        z = analogRead(15);
-        y = analogRead(16);
-        coolNumber = sqrt(pow(x,2) + pow(y,2) + pow(z,2));
-        // Serial.println("x:" + x +  "y: " +   y + "z: " + z);
-        Serial.println(coolNumber);
 
-        // Serial.printf("y is %d, down is %d, up is %d",x, yDown, yUp);
+void powerDOWN(const float *chord, float velocity){
+        if (chord[0] > 20.0) string1.noteOn(chord[0], velocity);
+        if (chord[1] > 20.0) string2.noteOn(chord[1], velocity);
+        if (chord[2] > 20.0) string3.noteOn(chord[2], velocity);
+        if (chord[3] > 20.0) string4.noteOn(chord[3], velocity);
+        if (chord[4] > 20.0) string5.noteOn(chord[4], velocity);
+        if (chord[5] > 20.0) string6.noteOn(chord[5], velocity);
+}
+long total1,total2,total3,total4, total5;
+void loop() {
+
+
+
+        const float *chord;
+        x = analogRead(accelorometerPinX);
+        z = analogRead(accelorometerPinZ);
+        y = analogRead(accelorometerPinY);
+        combi = sqrt(pow(x,2) + pow(y,2) + pow(z,2));
+        // Serial.println("x:" + x +  "y: " +   y + "z: " + z);
+        // Serial.println(coolNumber);
+        // Serial.println( coolNumber+ down+ mid+ up);
+        // Serial.printf("cool is %d, down is %d, mid is %d,  up is %d",coolNumber, down, mid, up);
+
         // Serial.println();
         // Serial.clear();
         // Serial.println(y);
-        chord = em7;
-        // if (counter >= 9){
-        //   yList[counter] = y;
-        //   counter++;
-        //   Serial.println(yList);
+        // chord = em7;
+        // **********************************************************************
+
+        total1 =  button1.capacitiveSensor(sampleSizeForCapacitiveSensor);
+        total2 =  button2.capacitiveSensor(sampleSizeForCapacitiveSensor);
+        total3 =  button3.capacitiveSensor(sampleSizeForCapacitiveSensor);
+        total4 =  button4.capacitiveSensor(sampleSizeForCapacitiveSensor);
+        // total5 =  button5.capacitiveSensor(sampleSizeForCapacitiveSensor);
+        // Serial.printf("%d, %d, %d, %d, %d, ", total1, total2, total3,total4, total5);
+        // Serial.printf("t1 is %d", total1);
+        Serial.println();
+        //
+        // **********************************************************************
+        // BUTTONS
+        if (total1 > 80) {
+                chord = Bpow;
+                // chord = em7;
+                // powerDOWN(chord, 1.);
+                // strum_dn(chord, 1.0);
+                // delay(hand_delay);
+                // Serial.println("Bpow");
+        }
+        if (total2 > 80) {
+                chord = Dpow;
+                // chord = Gmajor;
+                // strum_dn(chord, 1.0);
+                // delay(hand_delay);
+                // Serial.println("Dpow");
+
+        } if (total3 > 80) {
+                chord = Epow;
+                // chord = Dsus4;
+                // strum_dn(chord, 1.0);
+                // delay(hand_delay);
+                // Serial.println("Epow");
+
+        } if (total4 > 80) {
+                chord = Fspow;
+
+                // chord = A7sus4;
+                // strum_dn(chord, 1.0);
+                // delay(hand_delay);
+                // Serial.println("Fspow");
+                //
+        }
+        // } if (total5 > 20) {
+        //         chord = Gpow;
+        //         strum_dn(chord, 1.0);
+        //         delay(hand_delay);
+        //         // Serial.println("Gpow");
         // }
-        // strum_up(chord, 1.0);
-        // if (x)
+        // **********************************************************************
+        // delay(10);
+
+
+        Serial.printf(" : combi is is %d, D=%d, M=%d U=%d", combi, down, mid, up);
+        // Serial.printf(" : combi is is %d, D=%d, M=%d U=%d,  x is %d, z is %d", combi, down, mid, up);
         if (played) {
-          // Serial.println("WAITING FOR RESET" + direction);
-          // Serial.printf("WIATING FOR RESET : y is %d, D=%d, M=%d U=%d,  x is %d, z is %d", y, yDown, yMid, yUp, x,z );
-          // Serial.println();
+
+                Serial.println("WAITING FOR RESET" + direction);
+                // Serial.printf("WIATING FOR RESET : combi is is %d, D=%d, M=%d U=%d,  x is %d, z is %d", combi, down, mid, up);
+                // Serial.println();
                 if (direction) {
-                        if (y > yMid) {
-                            played = false;
-                            // Serial.println("pLAYED SET TO FALSE");
+                        if (combi > mid) {
+                                played = false;
+                                // Serial.println("pLAYED SET TO FALSE");
 
                         }
                 }
                 else {
-                  if (y < yMid) {
-                      played = false;
-                      // Serial.println("pLAYED SET TO FALSE");
+                        if (combi < mid) {
+                                played = false;
+                                // Serial.println("pLAYED SET TO FALSE");
 
-                  }
+                        }
                 }
         }
         else {
-                if (y > yDown ) {
-                                              Serial.println();
+                if (combi > down ) {
+                        // Serial.println();
                         Serial.println("UP");
                         // Serial.printf("x is %d, D=%d, M=%d U=%d,  y is %d, z is %d", x, yDown, yMid, yUp, y,z );
                         // Serial.println();
@@ -162,8 +240,8 @@ void loop() {
                         delay(hand_delay);
                         played = true;
                 }
-                else if (y < yUp ) {
-                                              Serial.println();
+                else if (combi < up ) {
+                        // Serial.println();
                         Serial.println("DOWNN");
                         // Serial.printf("x is %d, D=%d, M=%d U=%d,  y is %d, z is %d", x, yDown, yMid, yUp, y,z );
                         // Serial.println();
@@ -174,6 +252,11 @@ void loop() {
                         played = true;
                 }
         }
+
+
+
+
+
         // Serial.clear();
         // Serial.println(y);
         // Serial.println(z);
@@ -234,14 +317,7 @@ void loop() {
 
 
 
-void powerDOWN(const float *chord, float velocity){
-  if (chord[0] > 20.0) string1.noteOn(chord[0], velocity);
-  if (chord[1] > 20.0) string2.noteOn(chord[1], velocity);
-  if (chord[2] > 20.0) string3.noteOn(chord[2], velocity);
-  if (chord[3] > 20.0) string4.noteOn(chord[3], velocity);
-  if (chord[4] > 20.0) string5.noteOn(chord[4], velocity);
-  if (chord[5] > 20.0) string6.noteOn(chord[5], velocity);
-}
+
 
 
 
